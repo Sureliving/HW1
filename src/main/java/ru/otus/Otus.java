39faaa5d3b52f9f5986b19588c8024678d26f572
Alexandr Aliev <sureliving@gmail.com>
2021-06-21 17:21:45 +0500

    feat: commit intermediate

--- src/main/java/ru/otus/Otus.java
+++ src/main/java/ru/otus/Otus.java
@@ -2,6 +2,7 @@ package ru.otus;
 
 
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Before;
@@ -12,6 +13,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static org.junit.Assert.assertEquals;
 
@@ -23,7 +29,7 @@ public class Otus {
         Otus otus = new Otus();
         try {
             otus.setUp();
-            otus.testOtus();
+            otus.testPlan();
             logger.info("Test complete");
         } catch (Exception e) {
             logger.error("Error message", e);
@@ -31,40 +37,59 @@ public class Otus {
     }
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         System.setProperty(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY, "D:\\projects\\chromedriver_win32\\chromedriver.exe");
     }
 
-
     @Test
-    public void testOtus() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+    public void testPlan() throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(options);
+
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+
+//        Set implicit wait:
+//        wait for WebElement
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+//        wait for loading page
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
 
         driver.get("http://www.otus.ru/");
 
-        WebElement element = driver.findElement(By.xpath("/html/body/div[1]/div/header[1]/div/div[2]/div[1]/a[5]"));
+        WebElement element = driver.findElement(By.xpath("//div[@class='header2_subheader-container header2_subheader-container__inline']/a[@title='Контакты']"));
         JavascriptExecutor executor = (JavascriptExecutor)driver;
         executor.executeScript("arguments[0].click();", element);
 
+
         element = driver.findElement(By.xpath("//div[@class='c0qfa0-1 lblsQs']/div[@class='c0qfa0-5 cXQVNI']"));
-
-
-
         String actualAddress = element.getText();
-
         String expectedAddress = "125167, г. Москва, Нарышкинская аллея., д. 5, стр. 2, тел. +7 499 938-92-02";
-
         assertEquals(expectedAddress,actualAddress);
 
+
         driver.manage().window().maximize();
-        Thread.sleep(2000);
+
+
         String actualTitle = driver.getTitle();
-
-        String expectedTitle = "Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям";
-
+        String expectedTitle = "Контакты | OTUS";
         assertEquals(expectedTitle,actualTitle);
 
-        Thread.sleep(2000);  // Let the user actually see something!
+
+        driver.get("https://msk.tele2.ru/shop/number");
+        element = driver.findElement(By.xpath("//div[@class='text-field-holder']/input[@class='text-field']"));
+        element.sendKeys("97");
+        WebElement expectedElement = driver.findElement(By.xpath("//div[@class='numbers-slider-wrap']/div[@class='numbers-slider']"));
+        wait.until(webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
+
+        driver.get("http://www.otus.ru/");
+        element = driver.findElement(By.xpath("//div[@class='header2_subheader-container header2_subheader-container__inline']/a[@title='FAQ']"));
+
+        executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", element);
+
+        Thread.sleep(5000);
 
         driver.quit();
 
